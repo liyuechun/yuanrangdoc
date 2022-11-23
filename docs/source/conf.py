c4fca36a49d3996c02711b3972d71674470dc91f
liyuechun <liyuechun2009@163.com>
2022-11-24 00:29:53 +0800

    文档初始化

---
+++ docs/source/conf.py
@@ -0,0 +1,30 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sphinx_pdj_theme
+
+project = '元壤 - Web3.0 时代 NFT+DAO 与用户服务的数字化工具'
+copyright = '2022, 元壤 - 一分钟搭建您专属的NFT/DAO数字化粉丝深度运营平台'
+author = '元壤'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+extensions = ['recommonmark','sphinx_markdown_tables']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = '中文'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_static_path = ['_static']
+
+html_theme = 'sphinx_pdj_theme'
+html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
---
+++ docs/source/index.rst
@@ -0,0 +1,29 @@
+.. 元壤数字藏品SaaS使用说明文档 documentation master file, created by
+   sphinx-quickstart on Wed Nov 23 21:40:19 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+元壤 - 一分钟搭建您您专属的NFT+DAO数字化运营平台!
+========================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: 使用说明:
+
+   windows-shortcuts.md
+   dierdanyuan.md
+
+.. toctree::
+  :maxdepth: 2
+  :caption: 使用说明:
+
+  windows-shortcuts.md
+  dierdanyuan.md
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
